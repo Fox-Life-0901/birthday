354ba9ed5358d539374fbca0481e42ae1f81f3f4
Fox-Life-0901 <88835918+Fox-Life-0901@users.noreply.github.com>
2021-08-12 22:18:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "二婶",
-        "i m lengzai",
-        "kokoda wish lu",
-        "身体健康",
-        "啊， 还有",
-        "生日快乐",
-        "生日快乐 ！！！",
-        "喜欢的话给个五星好评呗"
+        "滑板仔",
+        "i m ur 妹夫",
+        "ah boi jiao wo",
+        "Happy Birthday to you",
+        "May you have a long life and be blessed always",
+        "可是我要说",
+        "哥，你是我亲哥！",
+        "我要见见你妹妹"
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -22,14 +22,14 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "二婶": "./imgs/1.png",
-        "i m lengzai": "./imgs/2.png",
-        "kokoda wish lu": "./imgs/1.png",
-        "身体健康": "./imgs/1.png",
-        "啊， 还有": "./imgs/1.png",
-        "生日快乐": "./imgs/1.png",
-        "生日快乐 ！！！": "./imgs/1.png",
-        "喜欢的话给个五星好评呗": "./imgs/1.png"
+        "滑板仔": "./imgs/1.png",
+        "i m ur 妹夫": "./imgs/2.png",
+        "ah boi jiao wo": "./imgs/1.png",
+        "Happy Birthday to you": "./imgs/2.png",
+        "May you have a long life and be blessed always": "./imgs/1.png",
+        "可是我要说": "./imgs/2.png",
+        "哥，你是我亲哥！": "./imgs/1.png",
+        "我要见见你妹妹": "./imgs/2.png"
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
@@ -37,9 +37,9 @@ var config = {
         turn_on: "开始",
         play: "音乐",
         bannar_coming: "舞台",
-        balloons_flying: "放气氛！",
-        cake_fadein: "上蛋糕！",
-        light_candle: "放火！",
+        balloons_flying: "放气！",
+        cake_fadein: "上coklat！",
+        light_candle: "烧他！",
         wish_message: "生日快乐！",
         story: "SURPRISE MTFK",
     }
